e8984212d60d07c3eaf98f36744515967c5de637
xioperez01 <angie.xioperez@gmail.com>
2021-10-27 17:51:43 -0500

    Task_2

--- 0x00-TypeScript/task_1/js/main.ts
+++ 0x00-TypeScript/task_1/js/main.ts
@@ -11,11 +11,26 @@ interface Teacher {
 // New Teacher
 
 const teacher3: Teacher = {
-  firstName: 'John',
+  firstName: "John",
   fullTimeEmployee: false,
-  lastName: 'Doe',
-  location: 'London',
+  lastName: "Doe",
+  location: "London",
   contract: false,
 };
 
 console.log(teacher3);
+
+// Directors Interface
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+const director1: Directors = {
+  firstName: "John",
+  lastName: "Doe",
+  location: "London",
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+
+console.log(director1);

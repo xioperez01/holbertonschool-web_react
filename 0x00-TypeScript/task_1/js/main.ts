fd5d16325d5c6e2af34049ad2386a3ffddac4529
xioperez01 <angie.xioperez@gmail.com>
2021-10-27 17:49:31 -0500

    Task_1

---
+++ 0x00-TypeScript/task_1/js/main.ts
@@ -0,0 +1,21 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [propName: string]: any;
+}
+
+// New Teacher
+
+const teacher3: Teacher = {
+  firstName: 'John',
+  fullTimeEmployee: false,
+  lastName: 'Doe',
+  location: 'London',
+  contract: false,
+};
+
+console.log(teacher3);

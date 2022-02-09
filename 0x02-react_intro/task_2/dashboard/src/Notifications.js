40eab3d855be7eb927fffe8dbf54fb31cf2d7429
xioperez01 <angie.xioperez@gmail.com>
2022-02-09 17:37:50 -0500

    Task 2 continued

--- 0x02-react_intro/task_2/dashboard/src/Notifications.js
+++ 0x02-react_intro/task_2/dashboard/src/Notifications.js
@@ -1,10 +1,34 @@
 import React from "react";
 import "./Notifications.css";
+import closeIcon from "./close-icon.png";
+import { getLatestNotification } from "./utils";
 
 export const Notifications = () => {
   return (
     <div className="Notifications">
+      <button
+        style={{
+          border: "none",
+          backgroundColor: "white",
+          position: "absolute",
+          right: "20px",
+          top: "20px",
+          cursor: "pointer",
+        }}
+        aria-label="close"
+        onClick={() => console.log("Close button has been clicked")}
+      >
+        <img src={closeIcon} alt="Close" />
+      </button>
       <p>Here is the list of notifications</p>
+      <ul>
+        <li data-default>New course available</li>
+        <li data-urgent>New resume available</li>
+        <li
+          data-urgent
+          dangerouslySetInnerHTML={{ __html: getLatestNotification() }}
+        ></li>
+      </ul>
     </div>
   );
 };
--- 0x02-react_intro/task_2/dashboard/src/utils.js
+++ 0x02-react_intro/task_2/dashboard/src/utils.js
@@ -5,3 +5,7 @@ export const getFooterCopy = (isIndex) => {
   if (isIndex) return "Holberton School";
   else return "Holberton School main dashboard";
 };
+
+export const getLatestNotification = () => {
+  return "<strong>Urgent requirement</strong> - complete by EOD";
+};

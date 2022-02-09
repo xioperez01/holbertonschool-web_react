078133912e8f286caa55fbb900711bd039270a6e
xioperez01 <angie.xioperez@gmail.com>
2022-02-09 16:56:08 -0500

    Task 2

---
+++ 0x02-react_intro/task_2/dashboard/src/utils.js
@@ -0,0 +1,7 @@
+export const getFullYear = () => {
+  return new Date().getFullYear();
+};
+export const getFooterCopy = (isIndex) => {
+  if (isIndex) return "Holberton School";
+  else return "Holberton School main dashboard";
+};
